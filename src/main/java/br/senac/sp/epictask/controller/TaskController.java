d6d5a53037e330e39687b9d3fd07fc40a7204f18
joaocarloslima <joaocarloslima@me.com>
2023-11-24 23:18:12 -0200

    oauth

--- src/main/java/br/senac/sp/epictask/controller/TaskController.java
+++ src/main/java/br/senac/sp/epictask/controller/TaskController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -37,6 +39,12 @@ public class TaskController {
 		if (result.hasErrors()) return "task/form";
 		redirect.addFlashAttribute("message", "Tarefa cadastrada com sucesso");
 		repository.save(task);
+		return "redirect:/task";
+	}
+	
+	@DeleteMapping("{id}") // /task/{id}
+	public String delete(@PathVariable Long id){
+		repository.deleteById(id);
 		return "redirect:/task";
 	}
 
